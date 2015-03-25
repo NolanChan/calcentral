7d99d42b154b7071a2a2e8c552f16ee55b24559a
Nolan Chan <nolanchan@berkeley.edu>
2015-03-24 18:07:14 -0700

    JSON returned in camelCase

--- app/controllers/search_users_controller.rb
+++ app/controllers/search_users_controller.rb
@@ -3,13 +3,25 @@ class SearchUsersController < ApplicationController
   def search_users
     authorize(current_user, :can_view_as?)
     users_found = User::SearchUsers.new(id: params['id']).search_users
-    render json: { users: users_found }.to_json
+    render json: { users: to_camel_keys(users_found) }.to_json
   end
 
   def search_users_by_uid
     authorize(current_user, :can_view_as?)
     users_found = User::SearchUsersByUid.new(id: params['id']).search_users_by_uid
-    render json: { users: users_found }.to_json
+    render json: { users: to_camel_keys(users_found) }.to_json
+  end
+
+  private
+  def to_camel_keys(value)
+    case value
+      when Array
+        value.map { |v| to_camel_keys(v) }
+      when Hash
+        Hash[value.map { |k, v| [k.camelize(:lower), to_camel_keys(v)] }]
+      else
+        value
+    end
   end
 
 end
